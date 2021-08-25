405082c2c35bef457d7eb99bd4c73587d26cf41f
Muhammad Ismail <yahyaismail@gmail.com>
2021-08-25 16:58:18 -0400

    Add playBack speed functionality

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
 // Play & Pause ----------------------------------- //
 
@@ -106,6 +107,10 @@ function toggleMute() {
 
 // Change Playback Speed -------------------- //
 
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
+
 // Fullscreen ------------------------------- //
 
 // Event Listeners
@@ -116,3 +121,4 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
